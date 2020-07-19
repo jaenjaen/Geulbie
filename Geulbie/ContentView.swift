0581fc9b4df394851df3939861b3b6bd30ba3972
Jaennie <je028770@gmail.com>
2020-07-19 18:26:23 +0900

    Initial Commit

---
+++ Geulbie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Geulbie
+//
+//  Created by 이재은 on 2020/07/19.
+//  Copyright © 2020 DINO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
