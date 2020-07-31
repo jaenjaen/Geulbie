a921d34f4c3cc6ef2e98a3b529fbf6828fa232ba
Shin <smy11go@gmail.com>
2020-07-31 19:31:41 +0900

    [2020-07-31 19:31] DB 읽어오기 예제 추가

--- Geulbie/ContentView.swift
+++ Geulbie/ContentView.swift
@@ -10,8 +10,25 @@ import SwiftUI
 import Firebase
 
 struct ContentView: View {
+    @ObservedObject var text = Text(of: 4)
+    
     var body: some View {
-        Text("No title")
+        VStack(alignment: .leading, spacing: 10.0) {
+            HStack(alignment: .bottom) {
+                SwiftUI.Text(text.title)
+                    .font(.largeTitle)
+                Spacer()
+                SwiftUI.Text("\(text.price)열매")
+                    .font(.subheadline)
+            }
+            SwiftUI.Text(text.sample)
+                .font(.headline)
+            Divider()
+            SwiftUI.Text(text.content)
+                .font(.body)
+        }
+        .padding()
+        .frame(maxWidth: .greatestFiniteMagnitude, maxHeight: .greatestFiniteMagnitude, alignment: Alignment.topLeading)
     }
 }
 
---
+++ Geulbie/Text.swift
@@ -0,0 +1,34 @@
+//
+//  Text.swift
+//  Geulbie
+//
+//  Created by Shin on 2020/07/31.
+//  Copyright © 2020 DINO. All rights reserved.
+//
+
+import Firebase
+
+class Text: ObservableObject {
+    @Published var title: String = "Title"
+    @Published var sample: String = "Sample"
+    @Published var content: String = "Content"
+    @Published var price: Int = -1
+    
+    init(of index: Int) {
+        getText(of: index)
+    }
+    
+    func getText(of index: Int) {
+        let ref = Database.database().reference()
+        
+        ref.child("text").child(String(index)).observeSingleEvent(of: .value, with: { snapshot in
+            let value = snapshot.value as? NSDictionary
+            self.title = value?["textTitle"] as? String ?? "No title"
+            self.sample = value?["sampleText"] as? String ?? "No sample"
+            self.content = value?["textContent"] as? String ?? "No content"
+            self.price = Int(value?["textPrice"] as? String ?? "-2") ?? -3
+            //print("title of snapshot: \(self.title)")
+        })
+        //print("title after reading func: \(title)")
+    }
+}
