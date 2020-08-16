daea837a231c8c36b0934519f708f0e5637fad54
Jaennie <je028770@gmail.com>
2020-08-16 18:24:16 +0900

    [2020-08-16 18:24] Series 시트뷰 연결

---
+++ Geulbie/SeriesView.swift
@@ -0,0 +1,21 @@
+//
+//  SeriesViewG.swift
+//  Geulbie
+//
+//  Created by 이재은 on 2020/08/16.
+//  Copyright © 2020 DINO. All rights reserved.
+//
+
+import SwiftUI
+
+struct SeriesView: View {
+    var body: some View {
+        Text("시리즈뷰")
+    }
+}
+
+struct SeriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        SeriesView()
+    }
+}
